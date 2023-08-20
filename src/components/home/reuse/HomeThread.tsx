d8b4694123640eee1c6be0b7534d42c7dcaffe66
WEI TSE <curry900312@gmail.com>
2023-08-20 18:19:55 +0800

    fix HomeThread Group UI position

--- src/components/home/reuse/HomeThread.tsx
+++ src/components/home/reuse/HomeThread.tsx
@@ -44,93 +44,102 @@ const HomeThread = ({ post }: HomeThreadProps) => {
   }
 
   return (
-    <div className="flex cursor-pointer gap-3 rounded-md bg-gray-800 p-2">
-      <div>
-        <Link href={"/"}>
-          <Image
-            src={post.author.image}
-            alt="user-image"
-            width={50}
-            height={50}
-            className="rounded-full object-cover"
-            priority
-          />
-        </Link>
-      </div>
+    <div>
+      {/*Group Info*/}
       <div
-        className="flex flex-1 flex-col gap-2"
-        onClick={() => push(`/post/${post.id}`)}
+        className="ml-auto flex items-center gap-2 p-2 cursor-pointer"
+        onClick={() => push(`/group/${post.groupId}`)}
       >
-        <div className="flex items-center gap-2">
-          <h1 className="font-bold">{post.author.name}</h1>
-          <span className="ml-auto text-xs">
-            {dayjs(post.createdAt).fromNow()}
-          </span>
-        </div>
-        <div className="flex flex-col">
-          <div>
-            <p className="text-md">{post.content}</p>
-          </div>
-          {/* <div>
+        <div className="h-[50px] w-[50px]">
+          <AspectRatio ratio={1 / 1}>
             <Image
-              src={"https://github.com/shadcn.png"}
-              alt="post-image"
-              width={200}
-              height={200}
+              src={post.group.image}
+              alt="group-image"
+              fill
               priority
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              className="rounded-full object-cover"
             />
-          </div> */}
+          </AspectRatio>
         </div>
-        <div className="flex items-center gap-3">
-          <div className="flex items-center gap-2">
-            <span>{post.comments.length}</span>
-            <span>comments</span>
+        <span className="text-xl font-Rubik">{post.group.name}</span>
+      </div>
+      {/*Post Info*/}
+      <div className="pl-10">
+        <div className="flex cursor-pointer gap-3 rounded-md bg-gray-800 hover:bg-gray-700 p-2">
+          <div>
+            <Link href={"/"}>
+              <Image
+                src={post.author.image}
+                alt="user-image"
+                width={50}
+                height={50}
+                className="rounded-full object-cover"
+                priority
+              />
+            </Link>
           </div>
           <div
-            className={
-              !!isUserLikePost
-                ? "flex items-center gap-2 text-red-400"
-                : "flex items-center gap-2"
-            }
+            className="flex flex-1 flex-col gap-2"
+            onClick={() => push(`/post/${post.id}`)}
           >
-            <span>{post.likes.length}</span>
-            <span>likes</span>
+            <div className="flex items-center gap-2">
+              <h1 className="font-bold">{post.author.name}</h1>
+              <span className="ml-auto text-xs">
+                {dayjs(post.createdAt).fromNow()}
+              </span>
+            </div>
+            <div className="flex flex-col">
+              <div>
+                <p className="text-md">{post.content}</p>
+              </div>
+              {/* <div>
+                <Image
+                  src={"https://github.com/shadcn.png"}
+                  alt="post-image"
+                  width={200}
+                  height={200}
+                  priority
+                />
+              </div> */}
+            </div>
+            <div className="flex items-center gap-3">
+              <div className="flex items-center gap-2">
+                <span>{post.comments.length}</span>
+                <span>comments</span>
+              </div>
+              <div
+                className={
+                  !!isUserLikePost
+                    ? "flex items-center gap-2 text-red-400"
+                    : "flex items-center gap-2"
+                }
+              >
+                <span>{post.likes.length}</span>
+                <span>likes</span>
+              </div>
+            </div>
+          </div>
+          <div>
+            <DropdownMenu>
+              <DropdownMenuTrigger className="mr-2">
+                <MoreIcon />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                {user?.id === post.author.id ? (
+                  <>
+                    <DropdownMenuItem onClick={() => handleDeletePost()}>
+                      Delete post
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>Edit post</DropdownMenuItem>
+                  </>
+                ) : (
+                  <DropdownMenuItem>Report</DropdownMenuItem>
+                )}
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         </div>
-        <div className="ml-auto flex items-center gap-2">
-          <div className="h-[30px] w-[30px]">
-            <AspectRatio ratio={1 / 1}>
-              <Image
-                src={post.group.image}
-                alt="group-image"
-                fill
-                priority
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                className="rounded-full object-cover"
-              />
-            </AspectRatio>
-          </div>
-          <span>{post.group.name}</span>
-        </div>
-      </div>
-      <div>
-        <DropdownMenu>
-          <DropdownMenuTrigger className="mr-2">
-            <MoreIcon />
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            {user?.id === post.author.id ? (
-              <>
-                <DropdownMenuItem onClick={() => handleDeletePost()}>
-                  Delete post
-                </DropdownMenuItem>
-                <DropdownMenuItem>Edit post</DropdownMenuItem>
-              </>
-            ) : (
-              <DropdownMenuItem>Report</DropdownMenuItem>
-            )}
-          </DropdownMenuContent>
-        </DropdownMenu>
       </div>
     </div>
   );

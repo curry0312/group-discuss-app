cefaeeb47198adec8f2eceb62cf4bad3bc3789dd
WEI TSE <curry900312@gmail.com>
2023-08-22 16:26:12 +0800

    fix group image skeleton

--- src/components/group/reuse/Group_skeleton.tsx
+++ src/components/group/reuse/Group_skeleton.tsx
@@ -3,8 +3,8 @@ import { Skeleton } from "~/components/ui/skeleton";
 const Group_skeleton = () => {
   return (
     <div className="flex gap-1 font-Rubik text-white">
-      <div className="basis-1/5">
-        <Skeleton className="h-20 w-20 rounded-full bg-slate-800" />
+      <div className="basis-1/5 flex justify-center items-center">
+        <Skeleton className="h-16 w-16 rounded-full bg-slate-800" />
       </div>
       <div className="flex basis-4/5 flex-col gap-3 justify-center">
         <Skeleton className="h-8 w-[250px] bg-slate-800" />
